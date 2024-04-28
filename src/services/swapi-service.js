fcc3347a28ad9d30c2f3a74c49eb42f3fe90ed8e
Alexey Kornilov <alex@Noutbuk-Alexey.local>
2024-04-28 17:41:07 +0300

    new API

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,5 +1,6 @@
 export default class SwapiService {
-  _apiBase = "https://swapi.dev/api/";
+  // _apiBase = "https://swapi.dev/api";
+  _apiBase = "https://sw-api.starnavi.io/";
   _imageBase = "https://starwars-visualguide.com/assets/img";
 
   getResource = async (url) => {
